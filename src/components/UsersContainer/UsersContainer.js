af61c2a6cb8206c70df1eae1a74eda1fce1a9398
Anastasia Dosiak <nastia.dos@gmail.com>
2019-09-02 21:36:56 +0300

    EQ-12: improvements: UsersContainer

--- src/components/UsersContainer/UsersContainer.js
+++ src/components/UsersContainer/UsersContainer.js
@@ -8,9 +8,9 @@ export default class UsersContainer extends PureComponent {
   state = { allUsers: mockUsers, invitedUsers: [], isUserEditing: false, userEditingIds: [] };
   componentDidMount() {
     const allUsers = JSON.parse(localStorage.getItem("allUsers"))
-    allUsers && this.setState({allUsers :[...allUsers]})
+    allUsers && this.setState({ allUsers: [...allUsers] })
     const invitedUsers = JSON.parse(localStorage.getItem("invitedUsers"))
-    invitedUsers && this.setState({ invitedUsers :[...invitedUsers]})
+    invitedUsers && this.setState({ invitedUsers: [...invitedUsers] })
   }
   componentDidUpdate(_prevProps, prevState) {
     localStorage.setItem("allUsers", JSON.stringify(this.state.allUsers))
@@ -21,116 +21,118 @@ export default class UsersContainer extends PureComponent {
       allUsers: [{ firstName, lastName, id }, ...this.state.allUsers]
     });
   };
+  changeUserNames = (users, firstName, lastName, id) => users.map((user) => {
+    if (user.id === id) {
+      return { ...user, firstName, lastName }
+    } else {
+      return user
+    }
+  })
   handleEdit = (firstName, lastName, id) => {
     this.setState({
       allUsers:
-        this.state.allUsers.map((user) => {
-          if (user.id === id) {
-            return { ...user, firstName, lastName }
-          } else {
-            return user
-          }
-        })
-
+        this.changeUserNames(this.state.allUsers, firstName, lastName, id),
+      invitedUsers:
+        this.changeUserNames(this.state.invitedUsers, firstName, lastName, id)
     })
-  }
-  handleSaveInvitation = (firstName, lastName, id) => {
-    !this.state.invitedUsers.find(user => user.id === id) &&
-      this.setState({
-        invitedUsers: [
-          ...this.state.invitedUsers,
-          { firstName, lastName, id, invited: true, accepted: false }
-        ]
-      })
-  };
-  handleStartEditUser = (userEditingId) => {
-    this.setState({ isUserEditing: true, userEditingIds: [...this.state.userEditingIds, userEditingId] })
-  }
-  handleStopEditUser = (userEditingId) => {
-    this.setState({ isUserEditing: false, userEditingIds: [...this.state.userEditingIds.filter((id) => id !== userEditingId)] })
-  }
-  handleClearInvitations = () => {
-    this.setState({
-      invitedUsers: []
-    });
-  };
-  handleDeleteInvitation = (userId) => {
+}
+handleSaveInvitation = (firstName, lastName, id) => {
+  !this.state.invitedUsers.find(user => user.id === id) &&
     this.setState({
       invitedUsers: [
-        ...this.state.invitedUsers.filter(({ id }) => id !== userId)
+        ...this.state.invitedUsers,
+        { firstName, lastName, id, invited: true, accepted: false }
       ]
     })
+};
+handleStartEditUser = (userEditingId) => {
+  this.setState({ isUserEditing: true, userEditingIds: [...this.state.userEditingIds, userEditingId] })
+}
+handleStopEditUser = (userEditingId) => {
+  this.setState({ isUserEditing: false, userEditingIds: [...this.state.userEditingIds.filter((id) => id !== userEditingId)] })
+}
+handleClearInvitations = () => {
+  this.setState({
+    invitedUsers: []
+  });
+};
+handleDeleteInvitation = (userId) => {
+  this.setState({
+    invitedUsers: [
+      ...this.state.invitedUsers.filter(({ id }) => id !== userId)
+    ]
+  })
+}
+handleUserStatusChange = (id, isActive) => {
+
+  this.setState({
+    invitedUsers:
+      this.state.invitedUsers.map((user) => {
+        if (user.id === id) {
+          return { ...user, accepted: isActive }
+        } else {
+          return user
+        }
+      })
+
+  })
+}
+onDragEnd = result => {
+  const { draggableId, destination } = result;
+  if (!destination) {
+    return;
   }
-  handleUserStatusChange = (id, isActive) => {
 
-    this.setState({
-      invitedUsers:
-        this.state.invitedUsers.map((user) => {
-          if (user.id === id) {
-            return { ...user, accepted: isActive }
-          } else {
-            return user
-          }
-        })
-
-    })
+  if (destination.droppableId === "invitationList") {
+    const { firstName, lastName } = this.state.allUsers.find((user) => user.id === draggableId)
+    !this.state.userEditingIds.includes(draggableId) && this.handleSaveInvitation(firstName, lastName, draggableId)
   }
-  onDragEnd = result => {
-    const { draggableId, destination } = result;
-    if (!destination) {
-      return;
-    }
-
-    if (destination.droppableId === "invitationList") {
-      const { firstName, lastName } = this.state.allUsers.find((user) => user.id === draggableId)
-      !this.state.userEditingIds.includes(draggableId) && this.handleSaveInvitation(firstName, lastName, draggableId)
-    }
-  }
+}
 
 
-  render() {
-    return (
-      <section className="content">
-        <DragDropContext onDragEnd={this.onDragEnd}>
+render() {
+  return (
+    <section className="content">
+      <DragDropContext onDragEnd={this.onDragEnd}>
 
-          <Droppable droppableId="userCatalog">
-            {(provided, snapshot) => (
-              <>
-                <UsersCatalog
-                    userEditingIds={this.state.userEditingIds}
-                  onStartEdit={this.handleStartEditUser}
-                  onStopEdit={this.handleStopEditUser}
-                  isDragingOver={snapshot.isDraggingOver}
-                  innerRef={provided.innerRef}
-                  onSave={this.handleSaveInvitation}
-                  onAdd={this.handleAdd}
-                  users={this.state.allUsers}
-                  onEdit={this.handleEdit}
-                />
-                <div className="dnd-staff-placeholder">{provided.placeholder}</div>
-              </>
-            )}
-          </Droppable>
-          <Droppable droppableId="invitationList">
-            {(provided, snapshot) => (
-              <>
-                <InvitationList
-                  isDragingOver={snapshot.isDraggingOver}
-                  innerRef={provided.innerRef}
-                  invitedLength={this.state.invitedUsers.length}
-                  onStatusChange={this.handleUserStatusChange}
-                  onClear={this.handleClearInvitations}
-                  users={this.state.invitedUsers}
-                  onDeleteInvitation={this.handleDeleteInvitation}
+        <Droppable droppableId="userCatalog">
+          {(provided, snapshot) => (
+            <>
+              <UsersCatalog
+                userEditingIds={this.state.userEditingIds}
+                onStartEdit={this.handleStartEditUser}
+                onStopEdit={this.handleStopEditUser}
+                isDragingOver={snapshot.isDraggingOver}
+                innerRef={provided.innerRef}
+                onSave={this.handleSaveInvitation}
+                onAdd={this.handleAdd}
+                users={this.state.allUsers}
+                onEdit={this.handleEdit}
+              />
+              <div className="dnd-staff-placeholder">{provided.placeholder}</div>
+            </>
+          )}
+        </Droppable>
+        <Droppable droppableId="invitationList">
+          {(provided, snapshot) => (
+            <>
+              <InvitationList
+                isDragingOver={snapshot.isDraggingOver}
+                innerRef={provided.innerRef}
+                invitedLength={this.state.invitedUsers.length}
+                onStatusChange={this.handleUserStatusChange}
+                onClear={this.handleClearInvitations}
+                users={this.state.invitedUsers}
+                onDeleteInvitation={this.handleDeleteInvitation}
 
-                />
-                <div className="dnd-staff-placeholder">{provided.placeholder}</div>
-              </>
-            )}
+              />
+              <div className="dnd-staff-placeholder">{provided.placeholder}</div>
+            </>
+          )}
 
-          </Droppable>
-        </DragDropContext>
-      </section >
-    );
-  }
+        </Droppable>
+      </DragDropContext>
+    </section >
+  );
+}
 }
